7049dcea51c425a2fb14f7bc337051e5963d1821
Villinger <s224227@dtu.dk>
2024-02-26 11:26:30 +0100

    delete wrong models

--- Project_description.py
+++
@@ -1,6 +0,0 @@
-# Neuralt netværk med MNIST dataset
-
-# Bayesian Optimazation på hyperparameteres fx. hidden units og antal neurouner i lagene
-
-#Data structure description
-## https://www.kaggle.com/datasets/hojjatk/mnist-dataset?resource=download ##
